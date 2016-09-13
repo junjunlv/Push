e0fdf41d447eac9ae151b4243a39c85c4cd52cf7
张晓静 <zhangxiaojing@zhangxiaojingdemac-mini.local>
2016-09-13 14:44:19 +0800

    Initial Commit

---
+++ jPush/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  jPush
+//
+//  Created by 张晓静 on 16/9/13.
+//  Copyright © 2016年 jun-object-c. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ jPush/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  jPush
+//
+//  Created by 张晓静 on 16/9/13.
+//  Copyright © 2016年 jun-object-c. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
